3184401534fadd8ff7722cc7518511912d166cef
anfelo <af.osorio1341@gmail.com>
2017-07-24 19:22:26 -0500

    Added routes logic and mongo connection

--- app.js
+++ app.js
@@ -1,7 +1,48 @@
-'use strict';
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+var app = express();
+
+
+// mongodb connection
+mongoose.connect("mongodb://localhost:27017/bookworm");
+var db = mongoose.connection;
+
+// mongo error
+db.on('error', console.error.bind(console, 'connection error'));
+
+// use sessions for tracking logins
+app.use(session({
+  secret: 'treehouse love you',
+  resave: true,
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: db
+  })
+}));
+
+// make the user id available in templates
+app.use(function (req, res, next){
+  res.locals.currentUser = req.session.userId;
+  next();
+});
+
+// parse incoming requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// serve static files from /public
+app.use(express.static(__dirname + '/public'));
+
+// view engine setup
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+
+// include routes
+var routes = require('./routes/index');
+app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
